c04ac833f3b4a4be8ae313e12e3731292ebfa02f
백준영 <jupiterbjy@gmail.com>
2019-12-25 07:37:37 +0000

    Listing and basic output now working, will figure out message part later.

--- UI_Main.py
+++ UI_Main.py
@@ -7,49 +7,49 @@ from oc_logReader import Ui_MainWindow
 import fileReader
 
 # https://github.com/RavenKyu/OpenTutorials_PyQt/
+# https://blog.asimation.com/37/
 
-class item_Form(QWidget):
-    def __init__(self):
-        QWidget.__init__(self, flags=Qt.Widget)
-        layout = QBoxLayout(QBoxLayout.TopToBottom)
-        pb = QPushButton("Hello")
-        layout.addWidget(pb)
-        layout.setSizeConstraint(QBoxLayout.SetFixedSize)
-        self.setLayout(layout)
+# TODO: find way to apply conditional formatting on columns
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        
-        self.File_PushButton.clicked.connect(self.fileButtonClicked)
-        
-        item = QListWidgetItem(self.oc_listWidget)
-        customItem = item_Form()
-        item.setSizeHint(customItem.sizeHint())
-        self.oc_listWidget.setItemWidget(item, customItem)
-        self.oc_listWidget.addItem(item)
-        
-    
+
+        self.actionFile.triggered.connect(self.fileButtonClicked)
+
     def writeConsole(self, text):
-        self.Debug_textEdit.append(str(text))
+        self.item_textBrowser.append(str(text))
         
+    def items(self, itemList):
+        self.item = QTreeWidgetItem()
+        
+        for idx, i in enumerate(itemList):
+            self.item.setText(idx, i)
+            
+        return self.item
+
     def fileButtonClicked(self):
         f = QFileDialog.getOpenFileName(self)
         self.writeConsole(f[0])
-        self.out = fileReader.lineProcess(fileReader.fileRead)
-        for i in self.out:
-            self.writeConsole(i)
         
+        if f[0] != '':
+            out = fileReader.lineProcess(f[0])
+        
+            for i in out:
+                item = self.items(i)
+                self.oc_treeWidget.invisibleRootItem().addChild(item)
+
+
 def main():
-    
+
     app = QApplication(sys.argv)
 
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-    
-    
+
+
 if __name__ == '__main__':
     main()
--- fileReader.py
+++ fileReader.py
@@ -1,39 +1,123 @@
 import os
+import re
 import sys
+
+# TODO: use check freeze state from python_img_macro
+# TODO: create python script to grep all TODO in source codes.
+# TODO: subitem by y-m-d => h-m-s order
 
 def setwdir():
     os.chdir(sys.path[0])
-            
-def fileRead(location, mode='rt'):
-    try:
-        file = open(location, mode)
-    except Exception as err:
-        print(err)
-        return 0  #fail condition
+
+
+def fileLineCounter(file):
+    for idx, l in enumerate(file):
+        pass
+
+    return idx
+    
+    
+def numericToAlphabet(total, index):
+    def digit(num, base=10):
+        if num == 0:
+            return 0
+        else:
+            return digit(num//base) + 1
+    
+    out = '0'*(digit(total) - digit(index))
+    
+    if index == 0:
+        return out
     else:
-        return file
-
-def lineProcess(file):
+        return out + str(index)
+    
+    
+def openWrapper(loc, mode='rt'):
     try:
-        with file as f:
-            output = []
-            for line, text in enumerate(f):
-                if line < 50:
-                    tmp = text.split(',')
-                    #tmp[-2] = ''
-                    for txt in tmp:
-                        if 'url' in txt:
-                            continue
-                        elif 'Trace' in txt:
-                            continue
-                        else:
-                            b = '\\'
-                            a = txt.split(b*3)
-                            out = ''.join(a)
-                            output.append(out.replace('\\\\n', '\n\n'))
+        f = open(loc, mode)
 
-                    output.append('\n\n\n')
-    except Exception as exp
-        print(exp)
+    except FileNotFoundError:
+        return -1
+
+    else:
+        return f
+    
+
+def lineProcess(location, limit=-1):
+    
+    def swap(arr, idx1, idx2):
+        arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+        
+    # Won't happen when called via UI class
+    # -------------------------------------
+    file = openWrapper(location)
+    if file == -1:
+        print('filenotfound')
+        return 0
+    # -------------------------------------
+    
+    blacklist = ['.*reqId.*', '.*url.*']
+    output = []
+    file_end = fileLineCounter(openWrapper(location))
+    # did this because python didn't process 'with file' after tossing file to LineCounter
+    
+    with file as f:
+        
+        for line, text in enumerate(f):
+            
+            if line == limit:
+                break
+
+            text_arr = text.split(',"')
+            index = numericToAlphabet(file_end, line)
+            item = [index]
+
+            for idx, txt in enumerate(text_arr):
                 
+                # checking if txt contains blacklists
+                # ----------------------------------
+                remove = False
+                
+                for bl in blacklist:
+                    if re.match(bl, txt):
+                        remove = True
+                        
+                if remove:
+                    continue
+                else:
+                    txt = re.sub('"', '', txt)
+                # ----------------------------------
+
+                if re.match('message', txt):
+                    # Add more processing to message section
+                    msg = re.sub('message', '', txt)
+                    msg = msg.replace(':', '', 1)
+                    
+                    item.append(msg)
+
+                else:
+                    txt = re.sub('}', '', txt)
+                    txt = re.sub(':', ',', txt, 1)
+                    txt = re.sub('.*,', '', txt)
+                    item.append(str(txt))
+            
+            swap(item, 1, 2)
+            if item[3]=='':
+                item[3] = '--'
+            if item[5] == 'no app in context':
+                item[5] = '--'
+            output.append(item)
+
     return output
+    
+    
+# Testing function
+
+if __name__ == '__main__':
+    output = lineProcess('./log/owncloud.log', limit=30)
+    for txt in output:
+        for i in txt:
+
+            print('*' + i + '*')
+    
+    
